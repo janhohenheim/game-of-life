4830c99a6efc113086f31feaf70711791ab5bde4
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-07-06 20:08:29 +0200

    Add observer pattern

--- src/controller.rs
+++ src/controller.rs
@@ -1,11 +1,20 @@
 use generation::{Change, GenerationCalculator};
 use grid::Grid;
+use std::rc::{Rc, Weak};
+use std::cell::RefCell;
 
 pub trait Presenter {
-    fn set_present_change_fn(&mut self, fun: Fn(Change));
+    fn register_controller(&mut self, Weak<RefCell<Controller>>);
+    fn present_change(&mut self, change: Change);
 }
 
-pub trait Controller {}
+pub enum PresenterEvent {
+    Change(Change),
+}
+
+pub trait Controller {
+    fn notify(&mut self, event: PresenterEvent);
+}
 
 pub struct ControllerImpl<G: Grid> {
     presenter: Box<Presenter>,
@@ -13,12 +22,23 @@ pub struct ControllerImpl<G: Grid> {
 }
 
 impl<G: Grid> ControllerImpl<G> {
-    fn new(presenter: Box<Presenter>, generation_calculator: Box<GenerationCalculator<G>>) -> Self {
-        ControllerImpl {
+    fn new(
+        presenter: Box<Presenter>,
+        generation_calculator: Box<GenerationCalculator<G>>,
+    ) -> Rc<RefCell<Self>> {
+        let controller = Rc::new(RefCell::new(ControllerImpl {
             presenter,
             generation_calculator,
-        }
+        }));
+        let second = Rc::downgrade(&controller);
+        controller
+            .borrow_mut()
+            .presenter
+            .register_controller(second);
+        controller
     }
 }
 
-impl<G: Grid> Controller for ControllerImpl<G> {}
+impl<G: Grid> Controller for ControllerImpl<G> {
+    fn notify(&mut self, event: PresenterEvent) {}
+}
