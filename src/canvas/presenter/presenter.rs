2364c56c14f60fb02a40b360b79dadd3f308792e
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-07-12 19:03:52 +0200

    Start implementing presenter

--- src/canvas/presenter/presenter.rs
+++ src/canvas/presenter/presenter.rs
@@ -3,11 +3,37 @@ extern crate mockers;
 #[cfg(test)]
 use mockers_derive::mocked;
 
-#[cfg_attr(test, mocked)]
-trait View {}
+use crate::controller::{Controller, Presenter};
+use crate::generation_calculator::Change;
+use std::borrow::BorrowMut;
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
 
-struct Presenter {
+type ObservablePresenter = Weak<RefCell<Presenter>>;
+
+#[cfg_attr(test, mocked)]
+trait View {
+    fn register_presenter(&mut self, presenter: ObservablePresenter);
+}
+
+struct CanvasPresenter {
     view: Box<View>,
+}
+impl CanvasPresenter {
+    pub fn new(view: Box<View>) -> Rc<RefCell<Self>> {
+        let presenter = Rc::new(RefCell::new(CanvasPresenter { view }));
+        let second = Rc::downgrade(&presenter);
+        {
+            let presenter_guard = presenter.borrow_mut();
+            presenter_guard.view.borrow_mut().register_presenter(second);
+        }
+        presenter
+    }
+}
+impl Presenter for CanvasPresenter {
+    fn register_controller(&mut self, controller: Weak<RefCell<Controller>>) {}
+    fn init_board(&mut self, width: u32, height: u32) {}
+    fn present_changes(&mut self, changes: &[Change]) {}
 }
 
 #[cfg(test)]
