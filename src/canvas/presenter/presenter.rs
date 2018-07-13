bd1dd4da444bc2019d086598feb2c2c493fcb4f5
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-07-13 17:33:16 +0200

    Add some basic viewing PODs

--- src/canvas/presenter/presenter.rs
+++ src/canvas/presenter/presenter.rs
@@ -7,9 +7,25 @@ extern crate mockers;
 #[cfg(test)]
 use mockers_derive::mocked;
 
+#[derive(Debug)]
+pub struct Line {
+    from: Position,
+    to: Position,
+    colour: String,
+}
+
+#[derive(Debug)]
+pub struct Square {
+    width: u32,
+    height: u32,
+    origin: Position,
+    colour: String,
+}
+
 #[cfg_attr(test, mocked)]
 pub trait View {
-    fn init_board(&mut self, width: u32, height: u32);
+    fn init_board(&mut self, width: u32, height: u32, lines: &[Line], squares: &[Square]);
+    fn draw_squares(&mut self, squares: &[Square]);
 }
 
 pub struct CanvasPresenter {
@@ -28,4 +44,24 @@ impl Presenter for CanvasPresenter {
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::interactive_game::InteractiveGameMock;
+    use mockers::matchers::ANY;
+    use mockers::Scenario;
+
+    const WIDTH: u32 = 1000;
+    const HEIGHT: u32 = 800;
+
+    fn create_mock() -> (Scenario, ViewMock) {
+        let scenario = Scenario::new();
+        let view = scenario.create_mock_for::<View>();
+        (scenario, view)
+    }
+
+    #[test]
+    fn inits_empty_board() {
+        let (scenario, view) = create_mock();
+        let mut presenter = CanvasPresenter::new(Box::new(view));
+        presenter.init_board(WIDTH, HEIGHT, &Vec::new());
+    }
+
 }
