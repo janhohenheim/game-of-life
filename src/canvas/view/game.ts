b8814b191af1a24bc0b54b5fc733fcb56395068e
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-07-16 12:55:15 +0200

    Make the debug fn more robust
    I adjusted the function found at
    https://stackoverflow.com/a/17130415/5903309

--- src/canvas/view/game.ts
+++ src/canvas/view/game.ts
@@ -1,6 +1,4 @@
 const rust = import("../../../wasm_generated/game_of_life");
-// rust.then(m => m.greet("World!"));
-
 
 rust.then(rust => {
     const canvas = document.getElementById('game-board') as HTMLCanvasElement;
@@ -8,12 +6,26 @@ rust.then(rust => {
     const game = rust.Game.new();
 
     canvas.addEventListener('click', (e) => {
-        const rect = canvas.getBoundingClientRect();
-        const pos = {
-            x: e.clientX - rect.left,
-            y: e.clientY - rect.top
-        };
+        const pos = getMousePos(canvas, e);
         console.log(pos);
     });
     game.start();
 })
+
+class MousePosition {
+    x: number;
+    y: number;
+}
+
+function getMousePos(canvas: HTMLCanvasElement, e: MouseEvent): MousePosition {
+    const rect = canvas.getBoundingClientRect();
+    const scaleX = canvas.width / rect.width;
+    const scaleY = canvas.height / rect.height;
+    const x = (e.clientX - rect.left) * scaleX;
+    const y = (e.clientY - rect.top) * scaleY;
+
+    return {
+        x: Math.round(x),
+        y: Math.round(y)
+    }
+}
